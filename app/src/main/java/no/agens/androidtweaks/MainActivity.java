c60df20d371d5f3e5c2df5c4dcda8729ed38bd24
Blanka Kulik <kulik.blanka@gmail.com>
2017-08-31 13:22:52 +0200

    Remove unused activities from sample app

--- app/src/main/java/no/agens/androidtweaks/MainActivity.java
+++ app/src/main/java/no/agens/androidtweaks/MainActivity.java
@@ -3,27 +3,10 @@ package no.agens.androidtweaks;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 //import no.agens.androidtweakslibrary.Interface.TweakBinding;
-import no.agens.androidtweakslibrary.Models.Collection;
-import no.agens.androidtweakslibrary.Models.Tweak;
-import no.agens.androidtweakslibrary.Models.TweakBoolean;
-import no.agens.androidtweakslibrary.Models.TweakStore;
 
 public class MainActivity extends AppCompatActivity {
-    private static String TWEAK_STORE_NAME = "tweakStoreName";
-    private static String COLLECTION_ID = "collectionId";
-    private List<Collection> collections;
-    private TweakStore tweakStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
