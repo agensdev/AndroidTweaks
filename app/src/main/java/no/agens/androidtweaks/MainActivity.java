7ce1cdcb62769f178429a3dccef4059c4da58d50
Blanka Kulik <kulik.blanka@gmail.com>
2017-09-04 09:50:18 +0200

    Add new activities to library's manifest

--- app/src/main/java/no/agens/androidtweaks/MainActivity.java
+++ app/src/main/java/no/agens/androidtweaks/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import no.agens.androidtweakslibrary.Activities.TweakStoreActivity;
+
 //import no.agens.androidtweakslibrary.Interface.TweakBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, Class.forName("no.agens.androidtweakslibrary.Activities.TweakStoreActivity"));
+        Intent intent = new Intent(this, TweakStoreActivity.class);
         startActivity(intent);
     }
 
